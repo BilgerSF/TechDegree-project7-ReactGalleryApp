9f215182ec1aa80b973bf2891005f7306d4b7897
gerardo <rodbilgerudemy@gmail.com>
2020-10-19 16:46:41 -0500

    Build

---
+++ my-app/build/precache-manifest.c171a914d3d1db18cde4a1c4655132d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9bc239e09ae11c12c187cc3db5d87e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05495a91e02ffebdb227",
+    "url": "/static/css/main.0da7dd35.chunk.css"
+  },
+  {
+    "revision": "91b10347112ad5bf3e38",
+    "url": "/static/js/2.9d5e8c00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d5e8c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05495a91e02ffebdb227",
+    "url": "/static/js/main.de2e8c3c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
